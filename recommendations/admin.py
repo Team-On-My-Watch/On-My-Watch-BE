f151e5a7b2270746b87bec1f98c4689921851faf
John Parrish <Johnwillparrish3@gmail.com>
2022-08-13 15:55:51 -0400

    added to admin for testing

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Tag, Recommendation, Comment
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Recommendation)
+admin.site.register(Comment)

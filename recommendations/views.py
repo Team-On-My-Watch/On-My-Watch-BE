5a6a4ef1acd72ca36bbedbda1d65f55e22883fb8
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-13 16:52:08 -0400

    built comment view

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from recommendations.models import Recommendation
-from .serializers import RecommendationSerializer
+from recommendations.models import Recommendation, Comment
+from .serializers import CommentSerializer, RecommendationSerializer
 
 
 class RecommendationAddListView(generics.ListCreateAPIView):
@@ -9,3 +10,15 @@ class RecommendationAddListView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+# get recommendation and post comment
+class CommentAddView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(recommendation_id=self.kwargs["pk"].order_by('-created_at'))
+
+    def perform_create(self, serializer):
+        recommendation = get_object_or_404(Recommendation, pk=self.kwargs.get('pk'))
+        serializer.save(user=self.request.user, recommendation=recommendation)

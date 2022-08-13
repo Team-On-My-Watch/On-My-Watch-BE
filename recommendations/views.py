9034ef9e7aeed7be53748a595d7c58e6eaacd8c4
John Parrish <Johnwillparrish3@gmail.com>
2022-08-13 15:55:11 -0400

    views added

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from recommendations.models import Recommendation
+from .serializers import RecommendationSerializer
 
-# Create your views here.
+
+class RecommendationAddListView(generics.ListCreateAPIView):
+    queryset = Recommendation.objects.all()
+    serializer_class = RecommendationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

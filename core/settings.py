4cec791fe3e9d9861417d3c4c1e9c6dd54ed713f
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-12 22:13:20 -0400

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -134,9 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-#https://stackoverflow.com/questions/26829435/collectstatic-command-fails-when-whitenoise-is-enabled
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

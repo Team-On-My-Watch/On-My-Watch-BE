4ed33cf501e01e939c471311c4d6683ffa04c56a
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-12 22:11:55 -0400

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 import django_on_heroku
 
 env = environ.Env(
@@ -133,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+#https://stackoverflow.com/questions/26829435/collectstatic-command-fails-when-whitenoise-is-enabled
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

8774fdfbf388ced9a7d9e72c2ad27f51ba262710
John Parrish <Johnwillparrish3@gmail.com>
2022-08-13 15:53:45 -0400

    added to models

--- recommendations/models.py
+++ recommendations/models.py
@@ -18,21 +18,27 @@ class BaseModel(models.Model):
 
 
 class Tag(BaseModel):
-    tags = models.TextField(max_length=500)
+    tags = models.TextField(max_length=50)
 
 
 class Recommendation(BaseModel):
+    TV_SHOW = 'TVS'
+    MOVIE = 'M'
+    STATUS_CHOICES = [
+        (TV_SHOW, 'TV Show'), (MOVIE, 'Movie'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recommendations')
     reason = models.TextField(max_length=750)
-    favorite = models.ManyToManyField(User, related_name='favorites', blank=True)
+    favorited_by = models.ManyToManyField(User, related_name='favorites', blank=True)
     imdbid = models.TextField(max_length=100)
     title = models.CharField(max_length=125)
-    medium = models.CharField(max_length=50)
+    medium = models.CharField(max_length=3, choices=STATUS_CHOICES)
     genre = models.CharField(max_length=200)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='user_tags')
+    tag = models.ManyToManyField(Tag, related_name='user_tags')
     description = models.TextField(max_length=1000)
     streaming_service = models.CharField(max_length=50, null=True, blank=True)
-    poster = models.ImageField(max_length=100)
+    poster = models.ImageField(max_length=100, null=True)
 
 
 class Comment(BaseModel):

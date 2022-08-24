4cb4989c68067d8d409cabbb74f0eed501863e51
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-24 15:55:29 -0400

    photo upload working locally

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Tag, Recommendation, Comment, Follow, UploadImageTest
+from .models import User, Tag, Recommendation, Comment, Follow
 
 
 admin.site.register(User)
@@ -7,4 +7,3 @@ admin.site.register(Tag)
 admin.site.register(Recommendation)
 admin.site.register(Comment)
 admin.site.register(Follow)
-admin.site.register(UploadImageTest)
--- recommendations/models.py
+++ recommendations/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    image = models.ImageField(upload_to='profile_photos', blank=True, null=True)
 
     def __str__(self):
         return self.username
@@ -58,11 +59,3 @@ class Follow(models.Model):
 
     def __str__(self):
         return f'{self.follower} follows {self.followee}'
-
-
-def nameFile(instance, filename):
-    return '/'.join(['images', str(instance.name), filename])
-
-class UploadImageTest(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to=nameFile, blank=True, null=True)

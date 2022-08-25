af0dbd953bf9a5469d9a187457cf9b39a9ac72d2
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-24 21:27:37 -0400

    FileUploadParser magically disappeared from imports on views.

--- recommendations/views.py
+++ recommendations/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 from recommendations import permissions
 from rest_framework import filters
 from recommendations.permissions import IsOwner

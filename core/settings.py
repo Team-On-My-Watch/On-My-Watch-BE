e34f3eb2a9a7f61cace6dc08cf9932a5419efcba
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-08-29 19:22:35 -0400

    removed unused import on settings.py

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
-import os
 from corsheaders.defaults import default_headers
 import django_on_heroku
 
